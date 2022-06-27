def62e78bdcfd93331dbb4b4e465184bd21fae87
geoffreyjorissenpxl <geoffreyjorissen@msn.com>
2022-06-27 03:54:47 +0200

    Add: Detail view for Movies

--- MyMovieDb/Controllers/MovieController.cs
+++ MyMovieDb/Controllers/MovieController.cs
@@ -3,14 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using MyMovieDb.Data.Services.Contracts;
 
 namespace MyMovieDb.Controllers
 {
     public class MovieController : Controller
     {
+        private IMovieService _movieService;
+        public MovieController(IMovieService movieService)
+        {
+            _movieService = movieService;
+        }
         public IActionResult Index()
         {
+
             return View();
+        }
+
+        public async Task<IActionResult> Detail(int id)
+        {
+            var movie = await _movieService.GetDetails(id);
+            movie.Cast = await _movieService.GetMovieCast(id);
+
+            return View(movie);
         }
     }
 }
