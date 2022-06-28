e381f94ff205dedf6b3aeaa3a6cb76df8cd0be72
geoffreyjorissenpxl <geoffreyjorissen@msn.com>
2022-06-29 01:04:33 +0200

    Add: added functionality to retrieve details of a tv show

---
+++ MyMovieDb/Models/TVDetailViewModel.cs
@@ -0,0 +1,14 @@
+﻿using MyMovieDb.Domain;
+using MyMovieDb.Domain.TVSeries;
+using System.Collections.Generic;
+
+namespace MyMovieDb.Models
+{
+    public class TVDetailViewModel
+    {
+        public TVSerie TVSerie { get; set; }
+        public IList<TVSerieSearch> Recommendations { get; set; }
+        public IList<Cast> Cast { get; set; }
+        public Media Media { get; set; }
+    }
+}
